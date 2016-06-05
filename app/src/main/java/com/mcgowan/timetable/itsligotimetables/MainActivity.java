58fbf2637fad64f823d3707071f8ab495c5d51e2
Brian McGowan <mcgowan.b@gmail.com>
2016-06-05 14:40:31 +0100

    commit before looking at sharedpreferences fragment

--- app/src/main/java/com/mcgowan/timetable/itsligotimetables/MainActivity.java
+++ app/src/main/java/com/mcgowan/timetable/itsligotimetables/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; t his adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
