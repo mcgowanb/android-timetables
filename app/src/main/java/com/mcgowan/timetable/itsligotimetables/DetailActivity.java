45c8590735fcca224139b874a0a2340cf23aa2ea
Brian McGowan <mcgowan.b@gmail.com>
2016-07-03 14:37:05 +0100

    added floating action bar to details & removed from main activity

--- app/src/main/java/com/mcgowan/timetable/itsligotimetables/DetailActivity.java
+++ app/src/main/java/com/mcgowan/timetable/itsligotimetables/DetailActivity.java
@@ -2,10 +2,13 @@ package com.mcgowan.timetable.itsligotimetables;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -25,6 +28,22 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addFloatingActionBar();
+    }
+
+    /**
+     * adds listener for the floating action button on screen
+     */
+    private void addFloatingActionBar() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/mcgowan/timetable/itsligotimetables/MainActivity.java
+++ app/src/main/java/com/mcgowan/timetable/itsligotimetables/MainActivity.java
@@ -2,13 +2,11 @@ package com.mcgowan.timetable.itsligotimetables;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
 import com.mcgowan.timetable.itsligotimetables.sync.TimetableSyncAdapter;
 
 
@@ -42,14 +40,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
