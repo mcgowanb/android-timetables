0f3ec83d4d4458fbbf8ceef6a1c7efb8dfb92bf0
Brian McGowan <mcgowan.b@gmail.com>
2016-07-26 21:08:50 +0100

    added markdown layout and loaded github page in about info

--- app/src/main/java/com/mcgowan/timetable/itsligotimetables/AboutActivityFragment.java
+++ app/src/main/java/com/mcgowan/timetable/itsligotimetables/AboutActivityFragment.java
@@ -1,22 +1,39 @@
 package com.mcgowan.timetable.itsligotimetables;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import us.feras.mdv.MarkdownView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AboutActivityFragment extends Fragment {
 
+    private static final String MARKDOWN_URL = "https://raw.githubusercontent.com/mcgowanb/android-timetables/master/about.md";
+
     public AboutActivityFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        MarkdownView markdownView = (MarkdownView) getActivity().findViewById(R.id.about_markdownView);
+//        markdownView.loadMarkdown("## Hello Markdown");
+        markdownView.loadMarkdownFile(MARKDOWN_URL);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
