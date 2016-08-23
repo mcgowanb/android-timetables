1f76b8fa74ce8309023b676de9e986455fb517b3
Brian McGowan <mcgowan.b@gmail.com>
2016-08-23 22:35:06 +0100

    added today details fragment as a tab and using tab loader to show it into view

--- app/src/main/java/com/mcgowan/timetable/android/TabPagesAdapter.java
+++ app/src/main/java/com/mcgowan/timetable/android/TabPagesAdapter.java
@@ -1,9 +1,12 @@
 package com.mcgowan.timetable.android;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import com.mcgowan.timetable.android.data.TimetableContract;
 
 public class TabPagesAdapter extends FragmentPagerAdapter {
 
@@ -20,11 +23,17 @@ public class TabPagesAdapter extends FragmentPagerAdapter {
         // Return a PlaceholderFragment (defined as a static inner class below).
         switch (position) {
             case 0:
-                return new TimeTableTodayFragment();
+                Fragment fragment = new NextClassFragment();
+                Bundle bundle = new Bundle();
+                String uri  = TimetableContract.TimetableEntry
+                        .buildTimetableUri(107).toString();
+                bundle.putString("URI", uri);
+                fragment.setArguments(bundle);
+                return fragment;
             case 1:
+                return new TimeTableTodayFragment();
+            case 2:
                 return new TimeTableWeekFragment();
-//            case 2:
-//                return Tab3.newInstance(position + 1);
         }
         return null;
     }
@@ -32,18 +41,18 @@ public class TabPagesAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 3 total pages.
-        return 2;
+        return 3;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
         switch (position) {
             case 0:
-                return mContext.getString(R.string.title_today);
+                return mContext.getString(R.string.title_next);
             case 1:
+                return mContext.getString(R.string.title_today);
+            case 2:
                 return mContext.getString(R.string.title_week);
-//            case 2:
-//                return getString(R.string.title_section3).toUpperCase(l);
         }
         return null;
     }
